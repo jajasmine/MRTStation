3dbcad3d81f4848d1063cfc4c09c98a6c918ca87
soslab <soslab@soslabdeMacBook-Pro.local>
2016-05-18 23:27:22 +0800

    first commit

---
+++ MRTStation/MRTListViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MRTListViewCell.swift
+//  MRTStation
+//
+//  Created by soslab on 2016/5/12.
+//  Copyright © 2016年 soslab. All rights reserved.
+//
+
+import UIKit
+
+class MRTListViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MRTStation/MRTStationJSON.swift
@@ -0,0 +1,9 @@
+//
+//  MRTStationJSON.swift
+//  MRTStation
+//
+//  Created by soslab on 2016/5/11.
+//  Copyright © 2016年 soslab. All rights reserved.
+//
+
+import Foundation
